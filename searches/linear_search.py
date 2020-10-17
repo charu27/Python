59dd67622de5a47de09fea608e386e30fdd9712f
charu27 <38068680+charu27@users.noreply.github.com>
2020-10-17 17:43:33 +0530

    Update linear_search.py

--- searches/linear_search.py
+++ searches/linear_search.py
@@ -20,8 +20,8 @@ def linear_search(sequence: list, target: int) -> int:
     Examples:
     >>> linear_search([0, 5, 7, 10, 15], 0)
     0
-    >>> linear_search([0, 5, 7, 10, 15], 15)
-    4
+    >>> linear_search([0, 5, 7, 10, 15], 10)
+    3
     >>> linear_search([0, 5, 7, 10, 15], 7)
     2
     >>> linear_search([0, 5, 7, 10, 15], 6)
